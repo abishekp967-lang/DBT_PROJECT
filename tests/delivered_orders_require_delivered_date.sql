4bec3e4a0ccaa847c536f49caeebc1eec36bdc60
abishekp967-lang <abishekp967@gmail.com>
2026-01-03 11:03:14 +0530

    JenkinsFile

--- tests/delivered_orders_require_delivered_date.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  order_id,
-  order_status,
-  order_delivered_customer_ts,
-  order_purchase_ts
-FROM {{ ref('fct_orders') }}
-WHERE order_status = 'delivered'
-  AND order_delivered_customer_ts IS NULL
